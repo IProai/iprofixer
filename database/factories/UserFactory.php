9a5df40c3b2c3a4ec0deaa3bb25202f78a8188ca
IProai <almoutlakomar@gmail.com>
2026-07-30 17:35:48 +0400

    Fix Pint style violation in user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,7 +20,7 @@ final class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'preferred_locale' => 'en',
-            'email_verified_at' => new DateTimeImmutable(),
+            'email_verified_at' => new DateTimeImmutable,
             'password' => '$2y$12$4E5hKjVn6VwA3V7VfO2jSO4t6FXyVJkH0SxmwH9d8U7l4p3N6Yz1K',
             'is_active' => true,
             'remember_token' => Str::random(10),
