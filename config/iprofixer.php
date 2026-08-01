ce073971126f24e13cceb13e691b1de22a9c14ae
IProai <almoutlakomar@gmail.com>
2026-08-01 11:16:11 +0400

    feat: define RFQ SLA thresholds

--- config/iprofixer.php
+++ config/iprofixer.php
@@ -4,4 +4,8 @@ declare(strict_types=1);
 
 return [
     'rfq_operations_email' => env('RFQ_OPERATIONS_EMAIL'),
+    'rfq_sla' => [
+        'first_response_hours' => (int) env('RFQ_FIRST_RESPONSE_HOURS', 4),
+        'stale_contact_hours' => (int) env('RFQ_STALE_CONTACT_HOURS', 48),
+    ],
 ];
