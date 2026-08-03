d1280978be87bc455147dbe4ac9f7537bc8d1f20
IProai <almoutlakomar@gmail.com>
2026-08-03 09:08:45 +0400

    fix(auth): register admin access provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,8 +2,10 @@
 
 declare(strict_types=1);
 
+use App\Providers\AdminAccessServiceProvider;
 use App\Providers\AppServiceProvider;
 
 return [
     AppServiceProvider::class,
+    AdminAccessServiceProvider::class,
 ];
